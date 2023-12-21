573de90604f3c814b8a561a06834734fc6a174eb
RaXx00n <cody.hough@flemingcollege.ca>
2023-12-20 22:56:13 -0500

    Custom Drawer Content

--- App.js
+++ App.js
@@ -9,15 +9,46 @@ import Settings from './Settings.js';
 import Profile from './Profile.js';
 import Chat from './Chat.js';
 import Lobbies from './Lobbies.js';
+import {Image, Text, View} from 'react-native';
+import { DrawerItem } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
 
 const Drawer = createDrawerNavigator();
 
+const CustomDrawerContent = ({ navigation }) => (
+  <View style={{ flex: 1, padding: 20}}>
+    <DrawerItem
+      label="Profile"
+      onPress={() => navigation.navigate('Profile')}
+    />
+    <DrawerItem
+      label="Chat"
+      onPress={() => navigation.navigate('Chat')}
+    />
+    <DrawerItem
+      label="Lobbies"
+      onPress={() => navigation.navigate('Lobbies')}
+    />
+    <Text style={{ fontSize: 8, marginBottom: 10 }}>Seraphine is the best champion</Text>
+    <Image
+      source={require('./sera-apc.png')}
+      style={{width: 163.5, height: 329.5, borderRadius: 5, marginBottom:20}}
+    />
+    <DrawerItem
+      label="Settings"
+      onPress={() => navigation.navigate('Settings')}
+    />
+  </View>
+);
+
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator initialRouteName="Home"
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        >
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Settings" component={Settings} />
         <Drawer.Screen name="Profile" component={Profile} />
@@ -27,6 +58,6 @@ const App = () => {
       </Drawer.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
